d55684c521de6e1de3e1b75352805faf32d2cf6c
Kyilee Bell <bellkyilee@gmail.com>
2025-10-10 12:40:29 -0500

    added location packages and basic tracking for location

--- frontend/Vellora/app/components/tracking.tsx
+++ frontend/Vellora/app/components/tracking.tsx
@@ -1,8 +1,35 @@
 import {  View, Text  } from "react-native";
+import { useState, useEffect } from "react";
+import * as Location from 'expo-location';
 
-function Tracking() {
+function Tracking() { 
+    const [location, setLocation] = useState<Location.LocationObject | null>(null);
+    const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+    useEffect(() => {
+        async function getCurrentLocation() {
+            let { status } = await Location.requestForegroundPermissionsAsync(); // Request Location Permissions from Phone
+            if (status !== 'granted') { // Check if Granted
+                setErrorMsg('Permission to access location was denied. Please enable location.');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        }
+
+        getCurrentLocation();
+    }, []);
+
+    let text = 'Waiting...';
+    if (errorMsg) {
+        text = errorMsg;
+    } else if (location) {
+        text = JSON.stringify(location);
+    }
+
     return (
-    <Text>This is the Tracking Component</Text>
+    <Text>This is the Tracking Component, Location: {text}</Text>
     )
 }
 
