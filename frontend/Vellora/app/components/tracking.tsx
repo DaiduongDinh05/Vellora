b682d21c2e4de0765fe405a0412ea23ab866c66e
Kyilee Bell <bellkyilee@gmail.com>
2025-10-11 14:21:48 -0500

    added background location tracking, updated app.json for location, and updated packages

--- frontend/Vellora/app/components/tracking.tsx
+++ frontend/Vellora/app/components/tracking.tsx
@@ -1,44 +1,56 @@
 import {  View, Text  } from "react-native";
 import { useState, useEffect } from "react";
 import * as Location from 'expo-location';
+import * as TaskManager from 'expo-task-manager';
 
-function Tracking() { 
+const LOCATION_TASK = 'background-location-tracking';
+
+
+function startBackgroundTracking() { 
     const [location, setLocation] = useState<Location.LocationObject | null>(null);
     const [errorMsg, setErrorMsg] = useState<string | null>(null);
+    const [subscription, setSubscription] = useState<Location.LocationSubscription | null>(null);
 
-    useEffect(() => {
-        async function getCurrentLocation() {
-             const foregroundPermissions = await Location.requestForegroundPermissionsAsync(); // Request Location Permissions from Phone
+    async function startTracking() {
+        // Get Perms for Location Tracking
+            const foregroundPermissions = await Location.requestForegroundPermissionsAsync(); // Request Location Permissions from Phone, have to request Foreground before BG
             if (!foregroundPermissions) { // Check if Granted
                 setErrorMsg('Permission to access location was denied. Please enable location.');
                 return;
             }
 
-            let { status } = await Location.requestBackgroundPermissionsAsync(); // Request Background Location for Tracking
-            if (status !== "granted" ) {
+            let { status: backgroundPermssions } = await Location.requestBackgroundPermissionsAsync(); // Request Background Location for Tracking
+            if (backgroundPermssions !== "granted" ) {
                 setErrorMsg('Permission to access background location was denied, Please enable background location for best performance.')
                 return;
             }
 
-            let location = await Location.getCurrentPositionAsync({});
-            setLocation(location);
-        }
+            const locationSubscription = await Location.watchPositionAsync({            // Subscription for Position Watching
+            accuracy: Location.LocationAccuracy.BestForNavigation,                      // Most accurate for Nav
+            timeInterval: 5000,                                                         // 5000 ms,
+            distanceInterval: 10                                                        // 10 meters
+        }, 
+            (newLocation) => {
+                setLocation(newLocation);                                               // Update Location
+                console.log(newLocation)
+            },
+        );
+        setSubscription(locationSubscription);
+    };
 
-        getCurrentLocation();
+
+
+    useEffect(() => {
+        startTracking();
     }, []);
 
-    let text = 'Waiting...';
-    if (errorMsg) {
-        text = errorMsg;
-    } else if (location) {
-        text = JSON.stringify(location);
-    }
+
 
     return (
-    <Text>This is the Tracking Component, Location: {text}</Text>
+    <Text>This is the Tracking Component</Text>
     )
 }
 
 
 
-export default Tracking;
+export default startBackgroundTracking;
