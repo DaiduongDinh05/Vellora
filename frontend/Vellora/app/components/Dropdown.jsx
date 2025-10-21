ecad29e9046f29e1fe8e3f02d4b3ba2c89440f89
Sara Nurdil <SaraNurdil@my.unt.edu>
2025-10-20 20:23:10 -0500

    Cleaned up tracker; moved dropdown picker into its own component file; made picker more flexible by passing props

--- frontend/Vellora/app/components/Dropdown.jsx
+++ frontend/Vellora/app/components/Dropdown.jsx
@@ -1,14 +1,63 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import RNPickerSelect from 'react-native-picker-select'
 
-const Dropdown = () => {
+const Dropdown = ({ items, onValueChange, placeholder, value }) => {
   return (
-    <View>
-      <Text>Dropdown</Text>
+    <View className="bg-gray-100 rounded-lg-mb-4">
+      <RNPickerSelect 
+            onValueChange={onValueChange}
+            items = {items}
+            value={value}
+            placeholder={placeholder ? { label: placeholder, value: null } : {}}
+            style={pickerSelectStyles}
+
+            pickerProps={{
+                itemStyle: {
+                    color: 'black',
+                }
+            }}
+                
+        />
+
+        {value && (
+            <Text>You selected: {value}</Text>
+        )}
+            
     </View>
   )
 }
 
 export default Dropdown
 
-const styles = StyleSheet.create({})
+
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 8,
+    color: 'black',
+    backgroundColor: 'white',
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 8,
+    color: 'black',
+    backgroundColor: 'white',
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+  placeholder: {
+    color: 'gray',
+  },
+  inputIOSContainer: {
+    zIndex: 100,
+  },
+});
--- frontend/Vellora/app/tracking.tsx
+++ frontend/Vellora/app/tracking.tsx
@@ -3,16 +3,20 @@ import React, { useState } from 'react'
 import RNPickerSelect from 'react-native-picker-select'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import NoteInput from './components/NoteInput'
-// export const Dropdown = () => {
-//   return (
-    
-//   )
-// }
+import Dropdown from './components/Dropdown'
 
 const Tracking = () => {
 
-    const [value, setValue] = useState(null);
-    const [notes, setNotes] = useState('');
+  const [value, setValue] = useState(null);
+  const [notes, setNotes] = useState('');
+  const [vehicle, setVehicle] = useState(null);
+
+  const vehicleItems = [
+    {label: 'Personal Car (Toyota)', value: 'toyota'},
+    {label: 'Business Car (Mazda)', value: 'mazda'},
+    {label: 'Random Car (Nissan)', value: 'nissan'},
+
+  ]
   return (
     <SafeAreaView style={{flex: 1}}>
         <View style={{flex:1, padding: 20}}>
@@ -24,67 +28,18 @@ const Tracking = () => {
                 onChangeText={setNotes}
                 className=''
             />
-            <RNPickerSelect 
-                onValueChange={(value) => setValue(value)}
-                items = {[
-                    {label: 'Test1', value: 'test1'},
-                    {label: 'Test2', value: 'test2'},
-                    {label: 'Test3', value: 'test3'},
-
-                ]}
-                value={value}
-                placeholder={{label: 'Select something...', value: null}}
-                style={pickerSelectStyles}
-
-                pickerProps={{
-                    itemStyle: {
-                        color: 'black',
-                    }
-                }}
-                
+            <Dropdown 
+              placeholder="Select vehicle"
+              items={vehicleItems}
+              onValueChange={setVehicle}
+              value={vehicle}
+            
             />
-
-            {value && (
-                <Text>You selected: {value}</Text>
-            )}
             
         </View>
     </SafeAreaView>
 
-
-    
   )
 }
 
 export default Tracking
-
-const pickerSelectStyles = StyleSheet.create({
-  inputIOS: {
-    fontSize: 16,
-    paddingVertical: 12,
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 8,
-    color: 'black',
-    backgroundColor: 'white',
-    paddingRight: 30, // to ensure the text is never behind the icon
-  },
-  inputAndroid: {
-    fontSize: 16,
-    paddingHorizontal: 10,
-    paddingVertical: 8,
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 8,
-    color: 'black',
-    backgroundColor: 'white',
-    paddingRight: 30, // to ensure the text is never behind the icon
-  },
-  placeholder: {
-    color: 'gray',
-  },
-  inputIOSContainer: {
-    zIndex: 100,
-  },
-});
