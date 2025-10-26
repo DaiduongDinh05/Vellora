3d0ac8829a9a720870f63ea8daadecd65aef14e0
Sara Nurdil <SaraNurdil@my.unt.edu>
2025-10-25 21:24:08 -0500

    Created a curreny input component file

---
+++ frontend/Vellora/app/components/CurrencyInput.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CurrencyInput = () => {
+  return (
+    <View>
+      <Text>CurrencyInput</Text>
+    </View>
+  )
+}
+
+export default CurrencyInput
+
+const styles = StyleSheet.create({})
