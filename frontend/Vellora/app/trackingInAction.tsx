bb623199697e3bafd58cf28e4ae8bf730577efd1
Sara Nurdil <SaraNurdil@my.unt.edu>
2025-10-27 20:43:26 -0500

    Created tracking in action screen file

---
+++ frontend/Vellora/app/trackingInAction.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const trackingInAction = () => {
+    return (
+    <View>
+        <Text>trackingInAction</Text>
+    </View>
+    )
+}
+
+export default trackingInAction
+
+const styles = StyleSheet.create({})
