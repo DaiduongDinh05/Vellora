8671528c240052e90f2ee136293455af94e39520
DaiduongDinh05 <daiduongddinh@gmail.com>
2025-10-30 00:00:29 -0500

    Removing docs endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from app.api.v1.router import router as api_v1_router
+import os
 
-
-app = FastAPI(title="Vellora", version="1.0.0")
+app = FastAPI(title="Vellora", version="1.0.0", docs_url=None if os.getenv("ENV") == "Production" else "/docs")
 
 @app.get("/")
 def root():
