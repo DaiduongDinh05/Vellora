07a14629e0f95e636ac0da03ef847fa5ebf69541
DaiduongDinh05 <daiduongddinh@gmail.com>
2025-10-30 00:14:21 -0500

    Removing docs and redocs endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.api.v1.router import router as api_v1_router
 import os
 
-app = FastAPI(title="Vellora", version="1.0.0", docs_url=None if os.getenv("ENV") == "Production" else "/docs")
+app = FastAPI(title="Vellora", version="1.0.0", docs_url=None if os.getenv("ENV") == "Production" else "/docs", redoc_url=None if os.getenv("ENV") == "Production" else "/redoc")
 
 @app.get("/")
 def root():
