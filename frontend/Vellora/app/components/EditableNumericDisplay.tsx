79018fa1fc5a88eec2cf5b57ea174eeec9c91826
Sara Nurdil <SaraNurdil@my.unt.edu>
2025-10-30 23:57:37 -0500

    Decide whether the changing value is miles or money value

--- frontend/Vellora/app/components/EditableNumericDisplay.tsx
+++ frontend/Vellora/app/components/EditableNumericDisplay.tsx
@@ -1,12 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, KeyboardTypeOptions } from 'react-native'
+import React, {useState} from 'react'
+import { FontAwesome } from '@expo/vector-icons'
 
-const EditableNumericDisplay = () => {
-  return (
-    <View>
-      <Text>EditableNumericDisplay</Text>
-    </View>
-  )
+type Props = {
+    label: string;
+    value: string;
+    onChangeText: (text: string) => void;
+    unit: '$' | 'mi';
+}
+const EditableNumericDisplay: React.FC<Props> = ({ label, value, onChangeText, unit }) => {
+    const [isEditing, setIsEditing] = useState(false);
+
+    let keyboardType: KeyboardTypeOptions = 'numeric';
+    let prefix = '';
+    let suffix = '';
+
+    if (unit == '$'){
+        keyboardType = 'decimal-pad';
+        prefix = '$';
+    }
+    else {
+        suffix = ' mi';
+    }
+
+    return (
+        <View>
+        <Text>EditableNumericDisplay</Text>
+        </View>
+    )
 }
 
 export default EditableNumericDisplay
