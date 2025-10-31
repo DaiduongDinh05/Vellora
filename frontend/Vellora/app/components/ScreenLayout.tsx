91d37d3c0a24111d1054d907363aa3ec955b0f05
Sara Nurdil <SaraNurdil@my.unt.edu>
2025-10-31 00:39:49 -0500

    Pasted redundant layout settings into screenlayout; added a tripdetailsform component for repetitive forms across screens

--- frontend/Vellora/app/components/ScreenLayout.tsx
+++ frontend/Vellora/app/components/ScreenLayout.tsx
@@ -1,12 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, ScrollView, TouchableWithoutFeedback, Keyboard, View } from 'react-native'
 import React from 'react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-const ScreenLayout = () => {
-  return (
-    <View>
-      <Text>ScreenLayout</Text>
-    </View>
-  )
+
+type ScreenLayoutProps = {
+    children: React.ReactNode;              // scrollable content
+    footer: React.ReactNode;                // sticky footer content
+};
+
+// This is a reusable screen layout that has a scrollview in it for content and a sticky part
+// for the futtor and buttons. It also handles keyboard dismissal (tapping out of it) and safe area
+const ScreenLayout: React.FC<ScreenLayoutProps> = ({ children, footer }) => {
+    const insets = useSafeAreaInsets();
+
+    return (
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()} accessible={false}>
+            <View style={{ flex: 1, backgroundColor: '#fff' }}>
+
+                <ScrollView
+                    style={{flex: 1}}
+                    contentContainerStyle={{
+                        paddingBottom: 150,
+                        paddingTop: insets.top,
+                    }}
+                    indicatorStyle='black'
+                    persistentScrollbar={true}
+                    keyboardShouldPersistTaps="handled"
+                
+                >
+                    {children}
+                </ScrollView>
+
+            </View>
+
+            <View
+                className='w-full bg-white px-6 pt-4 border-t border-gray-300'
+                style={{
+                    position: 'absolute',
+                    bottom: 0,
+                    paddingBottom: insets.bottom == 0 ? 12 : insets.bottom,
+                }}
+            >
+                {footer}
+            </View>
+
+        </TouchableWithoutFeedback>
+    )
 }
 
 export default ScreenLayout
---
+++ frontend/Vellora/app/components/TripDetailsForm.tsx
@@ -0,0 +1,22 @@
+import { View } from 'react-native'
+import React from 'react'
+import { FontAwesome } from '@expo/vector-icons'
+import NoteInput from './NoteInput'
+import Dropdown from './Dropdown'
+import CurrencyInput from './CurrencyInput'
+import FilledAddressBox from './FilledAddressBox'
+
+
+type TripDtailsFormProps = {
+
+};
+
+const TripDetailsForm = () => {
+    return (
+        <View>
+        
+        </View>
+    )
+}
+
+export default TripDetailsForm
