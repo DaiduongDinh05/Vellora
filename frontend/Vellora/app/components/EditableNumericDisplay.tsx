d3bb3521f827d665991b053faeb6660739ce5b00
Sara Nurdil <SaraNurdil@my.unt.edu>
2025-10-30 23:43:23 -0500

    Created a file for the autocalculated distance and value component

---
+++ frontend/Vellora/app/components/EditableNumericDisplay.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const EditableNumericDisplay = () => {
+  return (
+    <View>
+      <Text>EditableNumericDisplay</Text>
+    </View>
+  )
+}
+
+export default EditableNumericDisplay
+
+const styles = StyleSheet.create({})
