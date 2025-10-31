f2e7af1a34559d45512ccbaca50e84bab982ecf0
Sara Nurdil <SaraNurdil@my.unt.edu>
2025-10-31 02:40:55 -0500

    Create a file for manual log screen; installed date picker packages

---
+++ frontend/Vellora/app/manualLogScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const manualLogScreen = () => {
+    return (
+        <View>
+        <Text>manualLogScreen</Text>
+        </View>
+    )
+}
+
+export default manualLogScreen
+
+const styles = StyleSheet.create({})
