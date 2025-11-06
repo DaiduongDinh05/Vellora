26c39d041138a62520af92ef75d01978d30e5ac9
saranurdil <78966698+saranurdil@users.noreply.github.com>
2025-11-06 13:50:02 -0600

    Created a current location mapbox map component file

---
+++ frontend/Vellora/app/components/UserLocationMap.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const UserLocationMap = () => {
+    return (
+        <View>
+            <Text>UserLocationMap</Text>
+        </View>
+    )
+}
+
+export default UserLocationMap
+
+// const styles = StyleSheet.create({})
