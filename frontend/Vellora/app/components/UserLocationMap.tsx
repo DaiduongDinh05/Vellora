8e0a07b8388b988d3e6d6fec7a830ce078e9f908
saranurdil <78966698+saranurdil@users.noreply.github.com>
2025-11-07 16:05:57 -0600

    Completed a mapbox current location reusable component

--- frontend/Vellora/app/components/UserLocationMap.tsx
+++ frontend/Vellora/app/components/UserLocationMap.tsx
@@ -1,14 +1,89 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import Mapbox from '@rnmapbox/maps'
+import * as Location from 'expo-location'
+
+const MAPBOX_KEY = process.env.EXPO_PUBLIC_API_KEY_MAPBOX_PUBLIC_ACCESS_TOKEN;
+Mapbox.setAccessToken(`${MAPBOX_KEY}`);
 
 const UserLocationMap = () => {
+    const [hasLocationPermission, setHasLocationPermission] = useState(false);
+    const [initialCoordinate, setInitialCoordinate] = useState<[number, number] | null>(null);
+
+    useEffect(() => {
+
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                console.log('Permission to access location was denied');
+                return;
+            }
+            setHasLocationPermission(true);
+
+            let location = await Location.getCurrentPositionAsync({
+                accuracy: Location.Accuracy.Highest,
+            });
+            setInitialCoordinate([location.coords.longitude, location.coords.latitude]);
+        })();
+    }, []);
+
+    if (!initialCoordinate || !hasLocationPermission){
+        return (
+            <View style={styles.centerContainer}>
+                <ActivityIndicator size="large" color="#4DBF69" />
+                <Text style={{ marginTop: 10 }}>Finding your location...</Text>
+            </View>
+        );
+    }
+
     return (
-        <View>
-            <Text>UserLocationMap</Text>
+        <View style={styles.container}>
+            <Mapbox.MapView 
+                style={styles.map}
+                // logoEnabled={false}
+                attributionEnabled={false}
+                styleURL={Mapbox.StyleURL.Street}
+                rotateEnabled={false}
+            >
+
+                <Mapbox.Camera 
+                    zoomLevel={16}
+                    centerCoordinate={initialCoordinate}
+                    animationMode={'none'}
+                    // animationDuration={2000}
+                    // followUserLocation={true}
+                    // followUserMode={Mapbox.UserTrackingMode.Follow}
+                
+                />
+
+                <Mapbox.UserLocation
+                    visible={true}
+                    animated={true}
+                    showsUserHeadingIndicator={true}
+                />
+            </Mapbox.MapView>
+
         </View>
-    )
-}
+    );
+};
 
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        overflow: 'hidden',
+        borderRadius: 16,
+    },
+    map: {
+        flex: 1,
+    },
+    centerContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f5f5f5',
+        borderRadius: 16,
+    }
+
+
+});
 export default UserLocationMap
-
-// const styles = StyleSheet.create({})
--- frontend/Vellora/app/tracking.tsx
+++ frontend/Vellora/app/tracking.tsx
@@ -9,6 +9,7 @@ import ScreenLayout from './components/ScreenLayout';
 import TripDetailsForm from './components/TripDetailsForm';
 import Button from './components/Button';
 import { vehicleItems, typeItems, rateItems } from '../app/constants/dropdownOptions';
+import UserLocationMap from './components/UserLocationMap';
 
 const MAPBOX_KEY = process.env.EXPO_PUBLIC_API_KEY_MAPBOX_PUBLIC_ACCESS_TOKEN;
 Mapbox.setAccessToken(`${MAPBOX_KEY}`);
@@ -49,9 +50,8 @@ const Tracking = () => {
       <Text className="text-3xl text-primaryPurple font-bold p-6">Live Track Current Trip</Text>
 
       {/* Temporary map display. TO BE CHANGED TO AN IMPLEMENTATION WITH CURRENT LOCATION VIA MAPBOX */}
-      <View style={{ height: 300, width: '100%', overflow: 'hidden' }}>
-        <Mapbox.MapView style = {{ flex: 1 }}/>
-
+      <View style={{ height: 300, width: '100%', borderRadius: 16, overflow: 'hidden' }}>
+        <UserLocationMap />
       </View>
 
       {/* Container for displaying trip value and distance */}
