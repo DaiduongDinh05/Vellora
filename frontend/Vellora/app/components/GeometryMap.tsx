374fade958fa9086b613de6f23eb772ce39c7c31
saranurdil <78966698+saranurdil@users.noreply.github.com>
2025-11-07 20:44:34 -0600

    Added a sample GeoJson; created a geometry map component file

---
+++ frontend/Vellora/app/components/GeometryMap.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const GeometryMap = () => {
+    return (
+        <View>
+            <Text>GeometryMap</Text>
+        </View>
+    )
+}
+
+export default GeometryMap
+
+const styles = StyleSheet.create({})
