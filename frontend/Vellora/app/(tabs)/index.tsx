7b34ee911dbf2b9469a2d11f2cb1592c3a0f95b5
saranurdil <78966698+saranurdil@users.noreply.github.com>
2025-11-11 14:59:53 -0600

    Moved the tracking component TEMPORARILY onto index screen

--- frontend/Vellora/app/(tabs)/index.tsx
+++ frontend/Vellora/app/(tabs)/index.tsx
@@ -1,5 +1,7 @@
 import { Text, View } from "react-native";
 import { Link } from "expo-router";
+import Tracking from "../components/tracking";
+
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center"
@@ -7,6 +9,8 @@ export default function Index() {
       <Text className="text-5xl text-primaryPurple font-bold">Welcome!</Text>
       <Link href="../tracking">Live track a trip</Link>
       <Link href="../manualLogScreen">Manually log a trip</Link>
+      <Link href="../trackingTest"></Link>
+      <Tracking></Tracking>
     </View>
   );
 }
