4b282879278abe626a3829d7bf1d5dbb892465ed
saranurdil <78966698+saranurdil@users.noreply.github.com>
2025-11-15 00:15:49 -0600

    Create a hook file

--- frontend/Vellora/app/index.tsx
+++ frontend/Vellora/app/index.tsx
@@ -1,8 +1,8 @@
-import { Text, View } from "react-native";
-import LoginPage from "./pages/LoginPage";
+// import { Text, View } from "react-native";
+// import LoginPage from "./pages/LoginPage";
 
-export default function Index() {
-  return (
-      <LoginPage />
-  );
-}
+// export default function Index() {
+//   return (
+//       <LoginPage />
+//   );
+// }
