d6b86c3823a964bcfed37121b95659d6fe37c754
Aseel-Sh <aseel.sh52@gmail.com>
2025-11-18 11:55:24 -0600

    Added base files, migration, and model fields + relation

---
+++ backend/app/modules/reports/models.py
@@ -0,0 +1,30 @@
+from datetime import date
+import enum
+import uuid
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.core.base import Base
+
+
+class ReportStatus(str, enum.Enum):
+    pending = "pending"
+    processing = "processing"
+    completed = "completed"
+    failed = "failed"
+
+class Report(Base):
+    __tablename__="reports"
+    id : Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    user_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), sa.ForeignKey("users.id", ondelete="CASCADE"), nullable=False, index=True)
+    start_date: Mapped[date] = mapped_column(sa.Date, nullable=False)
+    end_date: Mapped[date] = mapped_column(sa.Date, nullable=False)
+    status: Mapped[ReportStatus] = mapped_column(sa.Enum(ReportStatus, name="report_status"), default=ReportStatus.pending, nullable=False)
+    #nullable for now
+    file_url: Mapped[str | None] = mapped_column(sa.String(512), nullable=True)
+    file_name: Mapped[str | None] = mapped_column(sa.String(100), nullable=True)
+    requested_at: Mapped[sa.DateTime] = mapped_column(sa.DateTime(timezone=True), server_default=sa.func.now(), nullable=False)
+    completed_at: Mapped[sa.DateTime | None] = mapped_column(sa.DateTime(timezone=True), nullable=True)
+
+    user: Mapped["User"] = relationship("User", back_populates="reports")
+
