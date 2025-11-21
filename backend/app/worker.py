017cc0d632172ace4465bb322ffaeea84a43d690
Aseel-Sh <aseel.sh52@gmail.com>
2025-11-21 17:40:40 -0600

    Made worker generate the reports

--- backend/app/worker.py
+++ backend/app/worker.py
@@ -3,6 +3,18 @@ import json
 
 from app.aws_client import get_sqs_client
 from app.config import settings
+from app.infra.db import AsyncSessionLocal
+
+from app.modules.reports.repository import ReportRepository
+from app.modules.reports.service import ReportsService
+
+from app.modules.reports.models import Report
+from app.modules.trips.models import Trip
+from app.modules.expenses.models import Expense
+from app.modules.users.models import User
+from app.modules.rate_categories.models import RateCategory
+from app.modules.rate_customizations.models import RateCustomization
+from app.modules.auth.models import RefreshToken
 
 
 class ReportWorker:
@@ -31,13 +43,22 @@ class ReportWorker:
                 body = json.loads(message["Body"])
                 print(f"Received message: {body}")
 
-                # delete after processing
+                await self.process_report(body["report_id"])
+
                 self.sqs.delete_message(
                     QueueUrl=self.queue_url,
                     ReceiptHandle=message["ReceiptHandle"]
                 )
 
-                print(f"Deleted message: {body}\n")
+                print(f"Deleted message from queue: {body}\n")
+
+    async def process_report(self, report_id: str):
+        async with AsyncSessionLocal() as session:
+            service = ReportsService(session, ReportRepository())
+            print(f"Generating report for {report_id}...")
+            report = await service.generate_now(report_id)
+
+            print(f"Report generated: file={report.file_name}")
 
 
 async def main():
