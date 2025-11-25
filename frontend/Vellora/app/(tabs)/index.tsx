06814d39cfe75afbde4353a36eec8b08fb124573
saranurdil <78966698+saranurdil@users.noreply.github.com>
2025-11-24 22:35:59 -0600

    Minor changes to home screen

--- frontend/Vellora/app/(tabs)/index.tsx
+++ frontend/Vellora/app/(tabs)/index.tsx
@@ -1,16 +1,19 @@
 import { Text, View } from "react-native";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { getProviderAuthorizeUrl, login } from "../services/auth";
 // import Tracking from "../components/tracking";
 
 export default function Index() {
+  
   return (
-    <View className="flex-1 justify-center items-center"
+    <SafeAreaView className="flex-1 justify-start p-[25px]"
     >
-      <Text className="text-5xl text-primaryPurple font-bold">Welcome!</Text>
+      <Text className="text-5xl text-primaryPurple font-bold">Welcome back,</Text>
       <Link href="../tracking">Live track a trip</Link>
       <Link href="../manualLogScreen">Manually log a trip</Link>
       <Link href="../trackingTest"></Link>
       {/* <Tracking></Tracking> */}
-    </View>
+    </SafeAreaView>
   );
 }
