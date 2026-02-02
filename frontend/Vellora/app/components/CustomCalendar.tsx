af4d42b27ac313fda027d24857375e7f733c5a32
saranurdil <78966698+saranurdil@users.noreply.github.com>
2026-02-02 12:55:05 -0600

    Installed calendar libraries; added component file

---
+++ frontend/Vellora/app/components/CustomCalendar.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const CustomCalendar = () => {
+    return (
+        <View>
+            <Text>Calendar</Text>
+        </View>
+    )
+}
+
+export default CustomCalendar
