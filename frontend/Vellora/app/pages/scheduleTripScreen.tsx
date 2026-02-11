d85237da2107af6f52ae2866f18acb94a4bf092f
saranurdil <78966698+saranurdil@users.noreply.github.com>
2026-02-10 19:14:56 -0600

    Creade trip schedule screen

---
+++ frontend/Vellora/app/pages/scheduleTripScreen.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { useState } from 'react'
+
+const scheduleTripScreen = () => {
+    return (
+        <View>
+            <Text>scheduleTripScreen</Text>
+        </View>
+    )
+}
+
+export default scheduleTripScreen
