235836a56473eee84b8a4f7d2a11ca45720a748f
Kyilee Bell <bellkyilee@gmail.com>
2026-02-16 10:14:15 -0600

    Added stop notification and added notification services

--- frontend/Vellora/app/services/Notifications.ts
+++ frontend/Vellora/app/services/Notifications.ts
@@ -3,7 +3,13 @@ import { ApiError, handleResponse, checkToken } from "./helpers";
 import { fetch } from 'expo/fetch';
 import * as Notifications from 'expo-notifications';
 
-
+export type Notification = {
+    user_id?: string;
+    type: string;
+    title: string;
+    message: string;
+    trip_id?: string;
+}
 
 
 export async function registerDevice(deviceToken: Notifications.ExpoPushToken, token?: string): Promise<Notifications.ExpoPushToken> {
@@ -19,4 +25,32 @@ export async function registerDevice(deviceToken: Notifications.ExpoPushToken, t
     });
     
     return handleResponse<Notifications.ExpoPushToken>(response);
+}
+
+export async function notifyStartedMoving(token?: string): Promise<Notification> {
+    const authToken = await checkToken() || token;
+
+    const response = await fetch(`${API_BASE_URL}/notifications/events/user-started-moving`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${authToken}`
+        }
+    });
+
+    return handleResponse<Notification>(response);
+}
+
+export async function notifyStoppedMoving(token?: string): Promise<Notification> {
+    const authToken = await checkToken() || token;
+    
+    const response = await fetch(`${API_BASE_URL}/notifications/events/user-stopped-moving`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${authToken}`
+        }
+    });
+
+    return handleResponse<Notification>(response);
 }
