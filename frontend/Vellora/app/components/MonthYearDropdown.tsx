895df38d57bd493741801dc1a97b2be274ab96d5
Kyilee Bell <bellkyilee@gmail.com>
2026-02-26 17:14:58 -0600

    added usestate vars

--- frontend/Vellora/app/components/MonthYearDropdown.tsx
+++ frontend/Vellora/app/components/MonthYearDropdown.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from 'react-native';
 import MonthPicker from 'react-native-month-year-picker';
 
@@ -7,5 +7,12 @@ import MonthPicker from 'react-native-month-year-picker';
 
 
 const MonthYearDropdown = () => {
-    
+    const [date, showDate] = useState<Date>(new Date());
+    const [show, setShow] = useState<boolean>(false);
 }
+
+
+
+
+
+export default MonthYearDropdown;
